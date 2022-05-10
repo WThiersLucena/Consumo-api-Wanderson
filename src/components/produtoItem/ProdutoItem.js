31f55e543baa5c8f398bcbae742265b9d89dbc47
WThiersLucena <you@example.com>
2022-05-10 10:26:53 -0300

    V3 com img por produto

--- src/components/produtoItem/ProdutoItem.js
+++ src/components/produtoItem/ProdutoItem.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function ProdutoItem(props) {
+
+
+ function ProdutoItem(props) {
+
     const produto = props.produto || []
+
     return (
         <>
             <h1>Produto</h1>
+            <h3>{produto.nome}</h3>
             {
                 <div>
-                    
+                    <div>
+                        <img width="250" src={produto.imgUrl} />
+                    </div>
                     <h2>{produto.id} {produto.nome} R${produto.preco}</h2>
                     <Link to="/">Voltar para home</Link>
                 </div>
@@ -15,3 +22,5 @@ export default function ProdutoItem(props) {
         </>
     )
 }
+
+export default ProdutoItem
